7824185ec6ecd7256df997b50c56e95c1c12c5df
Иван Гайдуков <ivan.gaydukov2005@gmail.com>
2024-12-08 18:38:07 +0300

    added Model

---
+++ Calculator.Model/Calculator.cs
@@ -0,0 +1,39 @@
+﻿namespace Calculator.Model;
+public enum MathFunction
+{
+    Sin,
+    Cos,
+    Sqrt,
+    Floor,
+    Ceil,
+}
+public enum MathOperation
+{
+    Add,
+    Subtract,
+    Div,
+    Mul,
+    Mod,
+    Pow
+}
+
+public static class MathOperations
+{
+    public static readonly Dictionary<MathOperation, Func<double, double>> Functions = new Dictionary<MathOperation, Func<double, double>>
+        {
+            { MathFunction.Sin, Math.Sin },
+            { MathFunction.Cos, Math.Cos },
+            { MathFunction.Sqrt, Math.Sqrt },
+            { MathFunction.Floor, Math.Floor },
+            { MathFunction.Ceil, Math.Ceiling }
+        };
+    public static readonly Dictionary<MathOperation, Func<double, double, double>> Operations = new Dictionary<MathOperation, Func<double, double>>
+        {
+            { MathOperation.Add, (a,b) => { a + b; } },
+            { MathOperation.Subtract, (a,b) => { a - b; } },
+            { MathOperation.Mul, (a,b) => { a * b;} },
+            { MathOperation.Div, (a,b) => { a / b; } },
+            { MathOperation.Mod, (a,b) => { a % b; } },
+            { MathOperation.Pow, (a,b) => { Math.Pow(a,b); } }
+        };
+}
