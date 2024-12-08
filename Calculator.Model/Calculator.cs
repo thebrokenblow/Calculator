4b8e8a00d491a0887d0d18047afb4f061bb5dce5
Artem <Artem@DESKTOP-4BOPE1O>
2024-12-08 19:48:02 +0300

    Merge веток

--- Calculator.Model/Calculator.cs
+++ Calculator.Model/Calculator.cs
@@ -1,12 +1,14 @@
 ﻿namespace Calculator.Model;
+
 public enum MathFunction
 {
     Sin,
     Cos,
     Sqrt,
     Floor,
-    Ceil,
+    Ceil
 }
+
 public enum MathOperation
 {
     Add,
@@ -19,21 +21,22 @@ public enum MathOperation
 
 public static class MathOperations
 {
-    public static readonly Dictionary<MathOperation, Func<double, double>> Functions = new Dictionary<MathOperation, Func<double, double>>
-        {
-            { MathFunction.Sin, Math.Sin },
-            { MathFunction.Cos, Math.Cos },
-            { MathFunction.Sqrt, Math.Sqrt },
-            { MathFunction.Floor, Math.Floor },
-            { MathFunction.Ceil, Math.Ceiling }
-        };
-    public static readonly Dictionary<MathOperation, Func<double, double, double>> Operations = new Dictionary<MathOperation, Func<double, double,double>>
-        {
-            { MathOperation.Add, (a,b) => { a + b; } },
-            { MathOperation.Subtract, (a,b) => { a - b; } },
-            { MathOperation.Mul, (a,b) => { a * b;} },
-            { MathOperation.Div, (a,b) => { a / b; } },
-            { MathOperation.Mod, (a,b) => { a % b; } },
-            { MathOperation.Pow, (a,b) => { Math.Pow(a,b); } }
-        };
+    public static readonly Dictionary<MathFunction, Func<double, double>> Functions = new()
+    {
+        { MathFunction.Sin, Math.Sin },
+        { MathFunction.Cos, Math.Cos },
+        { MathFunction.Sqrt, Math.Sqrt },
+        { MathFunction.Floor, Math.Floor },
+        { MathFunction.Ceil, Math.Ceiling }
+    };
+
+    public static readonly Dictionary<MathOperation, Func<double, double, double>> Operations = new()
+    {
+        { MathOperation.Add, (a, b) => a + b },
+        { MathOperation.Subtract, (a, b) => a - b },
+        { MathOperation.Mul, (a, b) => a * b },
+        { MathOperation.Div, (a, b) => a / b },
+        { MathOperation.Mod, (a, b) => a % b },
+        { MathOperation.Pow, Math.Pow }
+    };
 }
